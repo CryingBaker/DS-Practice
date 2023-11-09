9382d3d50c42b36a6c48da1948899c7f7556a021
CryingBaker <atharvinfo1811@gmail.com>
2023-11-09 22:26:11 +0530

    Reversing an array

---
+++ Day1/reversearray.java
@@ -0,0 +1,18 @@
+public class reversearray {
+    public static int[] getReverse(int array[], int arrayLength){
+        int temp;
+        for(int i = 0; i<=arrayLength/2; i++){
+            temp = array[i];
+            array[i] = array[arrayLength-i-1];
+            array[arrayLength-i-1] = temp;
+        }
+        return array;
+    }
+    public static void main(String args[]){
+        int array[] ={1,4,7,11,13};
+        int reversearray[] = getReverse(array,array.length);
+        for(int i = 0 ; i<reversearray.length; i++){
+            System.out.print(reversearray[i]+"  ");
+        }
+    }
+}
